efc094aff7338b6a9e067ed07bc2f164c414051a
anoopemacs <anoopemacs@gmail.com>
2016-08-18 16:58:02 +0530

    ch7 using react state to hide completed tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,6 +11,17 @@ import Task from './Task.jsx';
 // new es6: https://toddmotto.com/react-create-class-versus-component/
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+  toggleHideCompleted(){
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
   handleSubmit(event) {
     event.preventDefault();
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
@@ -22,7 +33,11 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).focus();
   }
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -30,7 +45,11 @@ class App extends Component {
     return (
       <div className="container">
 	<header>
-	  <h1>Todo List</h1>
+	  <h1>Todo List ({this.props.incompleteCount})</h1>
+	  <label className="hide-completed">
+	    <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+	    Hide Completed Tasks
+	  </label>
 	  <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
 	    <input
 	       type="text"
@@ -51,10 +70,13 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
+/* below is helping in connecting meteor data to react through props, but how?*/
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
