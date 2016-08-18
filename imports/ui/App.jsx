3461b4a6536b61f7a7a40ba4c46133edd2f324a4
anoopemacs <anoopemacs@gmail.com>
2016-08-18 15:43:31 +0530

    ch4 added forms

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -10,6 +11,16 @@ import Task from './Task.jsx';
 // new es6: https://toddmotto.com/react-create-class-versus-component/
 
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+    });
+    ReactDOM.findDOMNode(this.refs.textInput).value = "";
+    ReactDOM.findDOMNode(this.refs.textInput).focus();
+  }
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -20,6 +31,14 @@ class App extends Component {
       <div className="container">
 	<header>
 	  <h1>Todo List</h1>
+	  <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+	    <input
+	       type="text"
+	       ref="textInput"
+	       placeholder="Type to add new tasks"
+	    />
+	  </form>
+	      
 	</header>
 
 	<ul>
@@ -36,6 +55,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
